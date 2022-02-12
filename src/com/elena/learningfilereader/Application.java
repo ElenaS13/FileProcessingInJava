a6840c4a58c08caa82634c7cfaede0a2f765e0bd
ElenaS1981 <elenasidyakina@gmail.com>
2022-02-12 12:13:03 -0800

    first commit

---
+++ src/com/elena/learningfilereader/Application.java
@@ -0,0 +1,34 @@
+package com.elena.learningfilereader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		File file = new File("test.txt");
+		
+		try(FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader)
+;) {
+			
+			String line = bufferedReader.readLine();
+			
+			while(line !=null) {
+				System.out.println(line);
+				line = bufferedReader.readLine();
+			}
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Problem reading file" + file.getName());
+		}
+
+	}
+
+}
